53fbfdfcfe83796e597ddf619d0bac3488944692
LEON0801 <ang.li80@mavs.uta.edu>
2015-10-19 23:18:05 -0500

    clean up the test case and check in the hello world in the main java file

--- testproject/src/main/java/HelloWorld.java
+++ testproject/src/main/java/HelloWorld.java
@@ -2,4 +2,8 @@
  * Created by angli_000 on 10/19/2015.
  */
 public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
 }
--- testproject/src/test/java/HelloWorldTest.java
+++ testproject/src/test/java/HelloWorldTest.java
@@ -2,7 +2,4 @@
  * Created by angli_000 on 10/19/2015.
  */
 public class HelloWorldTest {
-    public static void main(String[] args) {
-        System.out.println("Hello World!!");
-    }
 }
